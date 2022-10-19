5c7e34cff5e9a6ee68a800c4af5c2260c4927d13
oneElectron <one.electron109@protonmail.com>
2022-10-19 23:04:58 +0200

    can now create a folder

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::fs;
+use console;
+
+fn main() -> std::io::Result<()> {
+    let path = env::current_dir()?;
+    println!("The current directory is {}", path.display());
+    let cudir = path.to_str();
+    if fs::create_dir([cudir.unwrap(), "/out"].join("")).is_err(){
+        print!("{}", console::style("Failed to create directory").bold().red())
+    }
+    Ok(())
 }
