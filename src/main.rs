ff9461c8b68883d6cf1db1d8d408a57417054e4d
oneElectron <one.electron109@protonmail.com>
2022-10-20 22:55:11 +0200

    release 0.1.0

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,23 @@
+const TESTING:bool = false;
+pub mod template;
 
 fn main() -> std::io::Result<()> {
     // Create out folder
-    //let path = std::env::current_dir()?;
-    //let cudir = path.to_str();
-    //std::fs::create_dir([cudir.unwrap(), "/out"].join("")).expect("printed");
-
-    // Print all env variables to stdout
-    let mut path:String = String::new();
-    for (key, value) in std::env::vars() {
-        println!("{} = {}", key, value);
-        if key == "PATH" {
-            path = value;
-        }
-    }
-    let paths = path.split(":");
-    for itpath in paths {
-        println!("{}", itpath);
+    let path = std::env::current_dir()?;
+    let cudir = path.to_str();
+    if !TESTING {
+        std::fs::create_dir([cudir.unwrap(), "/out"].join("")).expect("printed");
     }
 
+    // pick template
+    print!("Which template would you like to use: ");
+    let template_name:String = String::from("iSci");
+    //std::io::stdin()
+    //                .read_line(&mut template_name)
+    //                .expect("Could not read input");
 
-
+    // copy template
+    template::copy_template(template_name);
     
     Ok(())
 }
--- src/template.rs
+++ src/template.rs
@@ -1,9 +1,20 @@
+
+
 pub fn copy_template(name:String) {
-  // find brew
-  let output = Command::new("brew")
-                     .arg("command")
-                     .output()
-                     .expect("Failed to execute command");
-  // find template folder
+  // find templates folder
+  let mut maintex = std::path::PathBuf::from("/opt/homebrew/Cellar/itex/0.0.1/templates/iSci/main.tex");
+  let mainbib = std::path::PathBuf::from("/opt/homebrew/Cellar/itex/0.0.1/templates/iSci/main.bib");
+  let makefile = std::path::PathBuf::from("/opt/homebrew/Cellar/itex/0.0.1/templates/iSci/Makefile");
+
+  // find current dir
+  let mut pwd = String::new();
+  for (key, value) in std::env::vars() {
+      if key == "PWD" { pwd = key; }
+  }
+  let pwd:std::path::PathBuf = std::path::PathBuf::from(pwd.clone());
+
   // copy template to current dir
+  std::fs::copy(maintex, pwd.with_file_name("main.tex"));
+  std::fs::copy(mainbib, pwd.with_file_name("main.bib"));
+  std::fs::copy(makefile, pwd.with_file_name("Makefile"));
 }
