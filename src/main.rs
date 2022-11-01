5409cd426a3ced9d2acffc3371fbfb9523abc010
oneElectron <one.electron109@protonmail.com>
2022-11-01 22:53:57 +0100

    version 0.3

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,20 @@ fn main() -> std::io::Result<()> {
     let cudir = path.to_str();
     std::fs::create_dir([cudir.unwrap(), "/out"].join("")).expect("printed");
 
+    // print args
+    let template_name = std::env::args().nth(1);
+    if template_name.is_none() {
+        println!("failed to parse or find template name");
+        panic!();
+    }
+    let template_name = template_name.unwrap();
+
     // pick template
-    let template_name = "/iSci";
-    //std::io::stdin()
-    //                .read_line(&mut template_name)
-    //                .expect("Could not read input");
+    let template_name = std::string::String::from(template_name);
 
     // copy template
-    template::find_templates_folder();
+    let template_name = template_name.replace("\n", "");
+    template::copy_template(template_name);
     
     Ok(())
 }
--- src/template.rs
+++ src/template.rs
@@ -1,5 +1,5 @@
 
-pub fn find_templates_folder() {
+pub fn find_templates_folder() -> std::result::Result<std::path::PathBuf, i32> {
   let cellar_path = std::process::Command::new("brew").arg("--Cellar").output();
   if cellar_path.is_err() {
     println!("failed to find homebrew is your path");
@@ -14,42 +14,59 @@ pub fn find_templates_folder() {
     let mut path_to_templates = std::path::PathBuf::from(cellar_path);
     
     path_to_templates.push("itex");
+    if !path_to_templates.is_dir() {
+      println!("itex is not a folder");
+      panic!();
+    }
     
     let versions = std::fs::read_dir(&path_to_templates);
     if versions.is_err(){
       println!("failed to read {:?}", path_to_templates.as_os_str());
     }
     let versions = versions.unwrap();
-    if !(versions.count() == 1) {
+    if versions.count() != 1 {
       println!("You have more than more version of itex installed");
       panic!();
     }
-    
+    let versions = std::fs::read_dir(&path_to_templates).unwrap();
+    for version in versions {
+      path_to_templates.push(version.unwrap().file_name());
+    }
+    path_to_templates.push("itex-templates");
 
-
+    return Ok(path_to_templates);
   }
+  return Err(1);
 }
 
-/*
-pub fn copy_template(name:&str) {
-  let mut path_to_templates = find_templates();
 
-  path_to_templates.push_str("/itex-templates");
-  path_to_templates.push_str(name);
+pub fn copy_template(name:std::string::String) {
+  let mut path_to_templates = find_templates_folder().unwrap();
+  path_to_templates.push(name);
+
+  println!("{:?}", &path_to_templates);
+
   let template_files = std::fs::read_dir(&path_to_templates).unwrap();
 
   // find current dir
-  let mut pwd = String::new();
+  let mut pwd:Option<std::path::PathBuf> = None;
   for (key, value) in std::env::vars() {
-      if key == "PWD" { pwd = value; }
+      if key == "PWD" { 
+        pwd = Some(std::path::PathBuf::from(value));
+      }
   }
-  pwd.push_str("/file.txt");
-  let pwd:std::path::PathBuf = std::path::PathBuf::from(pwd);
+  if !pwd.is_some() {
+    println!("could not find PWD");
+    panic!();
+  }
+
+  let mut pwd = pwd.unwrap();
+  pwd.push("file.txt");
 
   // copy template to current dir
   for file in template_files {
-    if std::fs::copy(file.as_ref().unwrap().path(), pwd.as_path().with_file_name(file.unwrap().file_name())).is_err() {
+    if std::fs::copy(file.as_ref().unwrap().path(), pwd.with_file_name(file.unwrap().file_name())).is_err() {
       println!("could not copy");
     }
   }
-}*/
+}
