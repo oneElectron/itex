bbb8941937f5cbc64ac2644e7210b039b1b38f4b
oneElectron <one.electron109@protonmail.com>
2022-11-03 09:45:52 +0100

    exit from homebrew early if itex is not found

--- src/template.rs
+++ src/template.rs
@@ -80,34 +80,32 @@ pub fn find_templates_folder() -> std::result::Result<std::path::PathBuf, i32> {
   if !cellar_path.is_err() {
     let cellar_path = std::string::String::from_utf8(cellar_path.unwrap().stdout.to_vec());
     if cellar_path.is_err() {
-      println!("error while decoding homebrew cellar path");
+      println!("{}", console::style("error while decoding homebrew cellar path").bold().red());
       panic!();
     }
     let cellar_path = cellar_path.unwrap().replace("\n", "");
     let mut path_to_templates = std::path::PathBuf::from(cellar_path);
     
     path_to_templates.push("itex");
-    if !path_to_templates.is_dir() {
-      println!("{}", console::style("itex is not a folder in homebrew's cellar").red().bold());
-      panic!();
-    }
     
-    let versions = std::fs::read_dir(&path_to_templates);
-    if versions.is_err(){
-      println!("failed to read {:?}", path_to_templates.as_os_str());
-    }
-    let versions = versions.unwrap();
-    if versions.count() != 1 {
-      println!("You have more than more version of itex installed");
-      panic!();
-    }
-    let versions = std::fs::read_dir(&path_to_templates).unwrap();
-    for version in versions {
-      path_to_templates.push(version.unwrap().file_name());
-    }
-    path_to_templates.push("itex-templates");
+    if path_to_templates.is_dir() {
+      let versions = std::fs::read_dir(&path_to_templates);
+      if versions.is_err(){
+        println!("failed to read {:?}", path_to_templates.as_os_str());
+      }
+      let versions = versions.unwrap();
+      if versions.count() != 1 {
+        println!("{}", console::style("You have more than more version of itex installed").bold().red());
+        panic!();
+      }
+      let versions = std::fs::read_dir(&path_to_templates).unwrap();
+      for version in versions {
+        path_to_templates.push(version.unwrap().file_name());
+      }
+      path_to_templates.push("itex-templates");
 
-    return Ok(path_to_templates);
+      return Ok(path_to_templates);
+    }
   } // end if homebrew is found
 
   return Err(1);
