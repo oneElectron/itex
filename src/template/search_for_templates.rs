468a6058b11c82ff8333fb5e430a80f20c2e8676
oneElectron <one.electron109@protonmail.com>
2023-01-04 20:21:28 +0100

    fixed stuff

--- src/template/search_for_templates.rs
+++ src/template/search_for_templates.rs
@@ -1,3 +1,5 @@
+use std::ffi::OsString;
+
 pub fn search_in_homebrew() -> std::result::Result<std::path::PathBuf, i32> {
   let output = std::process::Command::new("brew").arg("-v").output();
   if output.is_err() {
@@ -9,8 +11,21 @@ pub fn search_in_homebrew() -> std::result::Result<std::path::PathBuf, i32> {
     eprintln!("Failed to run brew --Cellar and read the output");
     return Err(0);
   }
-  let mut cellar_path = std::path::PathBuf::from(cellar_path.unwrap());
+  let mut cellar_path = std::path::PathBuf::from(cellar_path.unwrap().trim());
   cellar_path.push("itex");
+  
+  let itex_dir = std::fs::read_dir(&cellar_path);
+  if itex_dir.is_err() {
+    println!("path: {}", cellar_path.to_str().unwrap());
+    println!("itex not found in homebrew");
+    panic!();
+  }
+  let itex_dir = itex_dir.unwrap();
+  let mut itex_version_number:OsString = Default::default();
+  for dl in itex_dir {
+    itex_version_number = dl.unwrap().file_name();
+  }
+  cellar_path.push(itex_version_number);
   println!("cellar path = {}", cellar_path.to_str().unwrap());
   cellar_path.push("itex-templates");
 
