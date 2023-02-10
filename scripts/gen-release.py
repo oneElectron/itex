153de220064f759ae6cc27d45e823e07e51c52ea
oneElectron <one.electron109@protonmail.com>
2023-02-10 22:58:51 +0100

    worked on gen-release

--- scripts/gen-release.py
+++ scripts/gen-release.py
@@ -2,6 +2,21 @@
 import libgithub as gh
 from libgithub import pretty_print as pp
 
-itex_repo = gh.Repo("oneElectron", "itex", gh.getAuthToken())
+itex_repo = gh.Repo("oneElectron", "test", gh.getAuthToken(), debuging=True)
 
 
+tag_name = input("Tag name: ")
+if not itex_repo.tagExists(tag_name):
+  print("Tag", tag_name, "does not exist")
+  exit()
+
+if itex_repo.releaseExists(tag_name):
+  print("Release already exists!")
+  exit()
+
+itex_repo.genRelease(tag_name)
+
+
+# TODO
+# publish release | get tag name, then upload release
+# zip and upload templates
--- scripts/libgithub.py
+++ scripts/libgithub.py
@@ -17,17 +17,51 @@ def pretty_print(input:dict):
 class Repo:
     token: str
     prefix: str
+    debug: bool
 
-    def __init__(self, user_name:str, repo_name:str, auth_token:str):
+    def __init__(self, user_name:str, repo_name:str, auth_token:str, debuging:bool = False):
         self.prefix = "https://api.github.com/repos/" + user_name + "/" + repo_name + "/"
         self.token = auth_token
+        self.debug = debuging
 
     def releases(self) -> dict:
         response_content = requests.get(self.prefix + "releases", headers={"Authorization": "Bearer " + self.token}).text
         out_dict = json.loads(response_content)
         return out_dict
+
+    def genRelease(self, tag_name: str):
+        release_data = {
+            "tag_name": tag_name,
+            "name": tag_name,
+            "draft": False,
+            "generate_release_notes": False,
+            "make_latest": False
+        }
+        json_data = json.JSONEncoder().encode(release_data)
+        print(json_data)
+        response = requests.post(self.prefix + "releases", json=json_data)
+        print(response.text)
+    
+    def releaseExists(self, release_name) -> bool:
+        response_content = requests.get(self.prefix + "releases", headers={"Authorization": "Bearer " + self.token}).text
+        data = json.loads(response_content)
+        for release in data:
+            if release["name"] == release_name:
+                return True
+        return False
     
     def tags(self) -> dict:
         response_content = requests.get(self.prefix + "tags", headers={"Authorization": "Bearer " + self.token}).text
         out_dict = json.loads(response_content)
         return out_dict
+    
+    def tagExists(self, tag_name:str) -> bool:
+        response_content = requests.get(self.prefix + "tags", headers={"Authorization": "Bearer " + self.token}).text
+        data = json.loads(response_content)
+        if self.debug:
+            pretty_print(data)
+        for tag in data:
+            if tag["name"] == tag_name:
+                return True
+        return False
+
