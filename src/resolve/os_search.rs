6463bd577b433db11b7edbf363d9493bf40d7ee7
Electron <one.electron109@protonmail.com>
2023-11-12 18:46:59 +0100

    Improve info module (#15)

--- src/resolve/os_search.rs
+++ src/resolve/os_search.rs
@@ -1,13 +1,15 @@
+use super::Error;
 use std::{env, path::PathBuf, process::Command, result::Result, string::String};
 
-pub fn search_in_homebrew() -> Result<PathBuf, i32> {
+#[cfg(unix)]
+pub(super) fn search_in_homebrew() -> Result<PathBuf, Error> {
     if Command::new("brew").output().is_err() {
-        return Err(1);
+        return Err(Error::NotFound);
     }
     let cellar_path = String::from_utf8(Command::new("brew").arg("--prefix").output().unwrap().stdout.to_vec());
     if cellar_path.is_err() {
         eprintln!("Failed to run brew --prefix and read the output");
-        return Err(0);
+        return Err(Error::NotFound);
     }
 
     let mut cellar_path = PathBuf::from(cellar_path.unwrap().trim());
@@ -16,13 +18,14 @@ pub fn search_in_homebrew() -> Result<PathBuf, i32> {
     cellar_path.push("itex-templates");
 
     if !cellar_path.is_dir() {
-        return Err(1);
+        return Err(Error::NotFound);
     }
 
     Ok(cellar_path)
 }
 
-pub fn search_in_unix() -> Result<PathBuf, i32> {
+#[cfg(unix)]
+pub(super) fn search_in_unix() -> Result<PathBuf, Error> {
     let home = env::var("HOME").expect("Could not find home");
 
     let path = PathBuf::from(home + "/.local/share/itex/itex-templates");
@@ -30,10 +33,11 @@ pub fn search_in_unix() -> Result<PathBuf, i32> {
         return Ok(path);
     }
 
-    Err(0)
+    Err(Error::NotFound)
 }
 
-pub fn search_in_windows() -> Result<PathBuf, i32> {
+#[cfg(windows)]
+pub(super) fn search_in_windows() -> Result<PathBuf, Error> {
     let mut app_data_dir = PathBuf::from(std::env::var("LOCALAPPDATA").expect("No App Data dir found"));
 
     app_data_dir.push("itex");
@@ -41,6 +45,6 @@ pub fn search_in_windows() -> Result<PathBuf, i32> {
     if app_data_dir.is_dir() {
         Ok(app_data_dir)
     } else {
-        Err(0)
+        Err(Error::NotFound)
     }
 }
