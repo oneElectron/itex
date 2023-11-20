0ba3a969703d350ab811401fc23f12b66d45a92e
Electron <one.electron109@protonmail.com>
2023-11-20 19:45:06 +0100

    Add the safe-build command (#30)

--- src/build.rs
+++ src/build.rs
@@ -1,8 +1,8 @@
 use crate::prelude::*;
 use std::io::{stdout, Write};
 
-pub fn build(debug: bool, draft_mode: bool) {
-    let mut settings = Settings::from_global();
+pub fn build(debug: bool, draft_mode: bool, settings: Option<Settings>) {
+    let mut settings = settings.unwrap_or(Settings::from_global());
 
     settings.check_tex_filename_is_set();
     settings.ensure_build_artifacts_path_exists();
@@ -19,16 +19,40 @@ pub fn build(debug: bool, draft_mode: bool) {
     pdflatex.run();
     let (pdflatex_output, _) = pdflatex.run();
 
-    if debug || !pdflatex_output.status.success() {
-        println!("{}", console::style("--- Bibtex Output ---").blue().bold());
-        stdout().write_all(&bibtex_output.stdout).unwrap();
+    if debug || settings.debug() || !pdflatex_output.status.success() {
         println!("{}", console::style("--- PDFLatex Output ---").blue().bold());
         stdout().write_all(&pdflatex_output.stdout).unwrap();
+        println!("{}", console::style("--- Bibtex Output ---").blue().bold());
+        stdout().write_all(&bibtex_output.stdout).unwrap();
+    }
+
+    if settings.clean() {
+        clean_out_folder(&settings);
     }
 
     if pdflatex_output.status.success() {
         copy_pdf_to_out_dir(&settings);
     }
+}
+
+fn clean_out_folder(settings: &Settings) {
+    let build_artifacts_path = settings.build_artifacts_path();
+    if build_artifacts_path.is_dir() {
+        std::fs::remove_dir_all(settings.build_artifacts_path()).unwrap();
+    }
+}
+
+pub fn safe_build() {
+    let mut settings = Settings::from_global();
+    settings.set_clean(Some(false));
+    settings.set_compile_bib(Some(true));
+    settings.set_draft_mode(Some(false));
+
+    clean_out_folder(&settings);
+
+    build(true, false, Some(settings.clone()));
+
+    build(true, false, Some(settings.clone()));
 }
 
 fn copy_pdf_to_out_dir(settings: &Settings) {
